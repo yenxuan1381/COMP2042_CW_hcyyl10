06cb68374f45735ec19b1e4c96f439aaee4a26f8
yenxuan1381 <emilylo1381@gmail.com>
2021-10-29 15:40:50 +0800

    added Javadocs and organised the files according to MVC pattern

---
+++ src/model/Ball.java
@@ -0,0 +1,219 @@
+package model;
+
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.awt.geom.RectangularShape;
+
+/**
+ * This abstract class Ball allows other class to implement its method
+ * @author filippo
+ *
+ */
+
+abstract public class Ball {
+
+    private Shape ballFace;
+
+    private Point2D center;
+
+    Point2D up;
+    Point2D down;
+    Point2D left;
+    Point2D right;
+
+    private Color border;
+    private Color inner;
+
+    private int speedX;
+    private int speedY;
+    
+    /**
+     * Constructor of Ball class
+     * 
+     * @param center The coordinates of the center of the ball
+     * @param radiusA The vertical radius of the ball
+     * @param radiusB The horizontal radius of the ball
+     * @param inner The color code for the inner color of the ball object
+     * @param border The color code for the border color of the ball object
+     */
+
+    public Ball(Point2D center,int radiusA,int radiusB,Color inner,Color border){
+        this.center = center;
+
+        up = new Point2D.Double();
+        down = new Point2D.Double();
+        left = new Point2D.Double();
+        right = new Point2D.Double();
+
+        up.setLocation(center.getX(),center.getY()-(radiusB / 2));
+        down.setLocation(center.getX(),center.getY()+(radiusB / 2));
+
+        left.setLocation(center.getX()-(radiusA /2),center.getY());
+        right.setLocation(center.getX()+(radiusA /2),center.getY());
+
+
+        ballFace = makeBall(center,radiusA,radiusB);
+        this.border = border;
+        this.inner  = inner;
+        speedX = 0;
+        speedY = 0;
+    }
+    
+    /**
+     * Abstract method to create the shape of the ball object
+     * @param center The coordinates of the center of the ball
+     * @param radiusA The vertical radius of the ball
+     * @param radiusB The horizontal radius of the ball
+     * @return The shape of the ball object
+     */
+
+    protected abstract Shape makeBall(Point2D center,int radiusA,int radiusB);
+    
+    /**
+     * Method to move the ball object 
+     */
+
+    public void move(){
+        RectangularShape tmp = (RectangularShape) ballFace;
+        center.setLocation((center.getX() + speedX),(center.getY() + speedY));
+        double w = tmp.getWidth();
+        double h = tmp.getHeight();
+
+        tmp.setFrame((center.getX() -(w / 2)),(center.getY() - (h / 2)),w,h);
+        setPoints(w,h);
+
+
+        ballFace = tmp;
+    }
+    
+    /**
+     * Method to set the speed of the ball
+     * @param x The horizontal speed of the ball
+     * @param y The vertical speed of the ball
+     */
+
+    public void setSpeed(int x,int y){
+        speedX = x;
+        speedY = y;
+    }
+    
+    /**
+     * Method to set the horizontal speed of the ball
+     * @param s The horizontal speed of the ball
+     */
+
+    public void setXSpeed(int s){
+        speedX = s;
+    }
+    
+    /**
+     * Method to set the vertical speed of the ball
+     * @param s The vertical speed of the ball
+     */
+    
+    public void setYSpeed(int s){
+        speedY = s;
+    }
+    
+    /**
+     * Method to reverse the horizontal speed of the ball (Move the ball in opposite direction horizontally)
+     */
+
+    public void reverseX(){
+        speedX *= -1;
+    }
+    
+    /**
+     * Method to reverse the vertical speed of the ball (Move the ball in opposite direction vertically)
+     */
+
+    public void reverseY(){
+        speedY *= -1;
+    }
+    
+    /**
+     * Getter for the border color of the ball object
+     * @return Color code of the border color of the ball 
+     */
+
+    public Color getBorderColor(){
+        return border;
+    }
+    
+    /**
+     * Getter for the inner Color of the ball object
+     * @return Color code of the inner color of the ball 
+     */
+
+    public Color getInnerColor(){
+        return inner;
+    }
+    
+    /**
+     * Getter for the position of the ball object
+     * @return the coordinates of the ball's position
+     */
+
+    public Point2D getPosition(){
+        return center;
+    }
+    
+    /**
+     * Getter for the shape of the ball object
+     * @return the shape of the ball <!!>
+     */
+    
+    public Shape getBallFace(){
+        return ballFace;
+    }
+    
+    /**
+     * Method to move the ball to point p
+     * @param p coordinates of the point for the ball to move to
+     */
+
+    public void moveTo(Point p){
+        center.setLocation(p);
+
+        RectangularShape tmp = (RectangularShape) ballFace;
+        double w = tmp.getWidth();
+        double h = tmp.getHeight();
+
+        tmp.setFrame((center.getX() -(w / 2)),(center.getY() - (h / 2)),w,h);
+        ballFace = tmp;
+    }
+    
+    /**
+     * Method to set the points of the ball
+     * @param width The width of the ball
+     * @param height The height of the ball
+     */
+
+    private void setPoints(double width,double height){
+        up.setLocation(center.getX(),center.getY()-(height / 2));
+        down.setLocation(center.getX(),center.getY()+(height / 2));
+
+        left.setLocation(center.getX()-(width / 2),center.getY());
+        right.setLocation(center.getX()+(width / 2),center.getY());
+    }
+    
+    /**
+     * Getter for the horizontal speed of the ball 
+     * @return the horizontal speed of the ball
+     */
+
+    public int getSpeedX(){
+        return speedX;
+    }
+    
+    /**
+     * Getter for the vertical speed of the ball
+     * @return the vertical speed of the ball
+     */
+
+    public int getSpeedY(){
+        return speedY;
+    }
+
+
+}
---
+++ src/model/Wall.java
@@ -0,0 +1,336 @@
+/*
+ *  Brick Destroy - A simple Arcade video game
+ *   Copyright (C) 2017  Filippo Ranza
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package model;
+
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.util.Random;
+
+
+public class Wall {
+
+    private static final int LEVELS_COUNT = 4;
+
+    private static final int CLAY = 1;
+    private static final int STEEL = 2;
+    private static final int CEMENT = 3;
+
+    private Random rnd;
+    private Rectangle area;
+
+    private Brick[] bricks;
+    private Ball ball;
+    private Player player;
+
+    private Brick[][] levels;
+    private int level;
+
+    private Point startPoint;
+    private int brickCount;
+    private int ballCount;
+    private boolean ballLost;
+
+    public Wall(Rectangle drawArea, int brickCount, int lineCount, double brickDimensionRatio, Point ballPos){
+
+        this.startPoint = new Point(ballPos);
+
+        levels = makeLevels(drawArea,brickCount,lineCount,brickDimensionRatio);
+        level = 0;
+
+        ballCount = 3;
+        ballLost = false;
+
+        rnd = new Random();
+
+        makeBall(ballPos);
+        int speedX,speedY;
+        do{
+            speedX = rnd.nextInt(5) - 2;
+        }while(speedX == 0);
+        do{
+            speedY = -rnd.nextInt(3);
+        }while(speedY == 0);
+
+        getBall().setSpeed(speedX,speedY);
+
+        setPlayer(new Player((Point) ballPos.clone(),150,10, drawArea));
+
+        area = drawArea;
+
+
+    }
+
+    private Brick[] makeSingleTypeLevel(Rectangle drawArea, int brickCnt, int lineCnt, double brickSizeRatio, int type){
+        /*
+          if brickCount is not divisible by line count,brickCount is adjusted to the biggest
+          multiple of lineCount smaller then brickCount
+         */
+        brickCnt -= brickCnt % lineCnt;
+
+        int brickOnLine = brickCnt / lineCnt;
+
+        double brickLen = drawArea.getWidth() / brickOnLine;
+        double brickHgt = brickLen / brickSizeRatio;
+
+        brickCnt += lineCnt / 2;
+
+        Brick[] tmp  = new Brick[brickCnt];
+
+        Dimension brickSize = new Dimension((int) brickLen,(int) brickHgt);
+        Point p = new Point();
+
+        int i;
+        for(i = 0; i < tmp.length; i++){
+            int line = i / brickOnLine;
+            if(line == lineCnt)
+                break;
+            double x = (i % brickOnLine) * brickLen;
+            x =(line % 2 == 0) ? x : (x - (brickLen / 2));
+            double y = (line) * brickHgt;
+            p.setLocation(x,y);
+            tmp[i] = makeBrick(p,brickSize,type);
+        }
+
+        for(double y = brickHgt;i < tmp.length;i++, y += 2*brickHgt){
+            double x = (brickOnLine * brickLen) - (brickLen / 2);
+            p.setLocation(x,y);
+            tmp[i] = new ClayBrick(p,brickSize);
+        }
+        return tmp;
+
+    }
+
+    private Brick[] makeChessboardLevel(Rectangle drawArea, int brickCnt, int lineCnt, double brickSizeRatio, int typeA, int typeB){
+        /*
+          if brickCount is not divisible by line count,brickCount is adjusted to the biggest
+          multiple of lineCount smaller then brickCount
+         */
+        brickCnt -= brickCnt % lineCnt;
+
+        int brickOnLine = brickCnt / lineCnt;
+
+        int centerLeft = brickOnLine / 2 - 1;
+        int centerRight = brickOnLine / 2 + 1;
+
+        double brickLen = drawArea.getWidth() / brickOnLine;
+        double brickHgt = brickLen / brickSizeRatio;
+
+        brickCnt += lineCnt / 2;
+
+        Brick[] tmp  = new Brick[brickCnt];
+
+        Dimension brickSize = new Dimension((int) brickLen,(int) brickHgt);
+        Point p = new Point();
+
+        int i;
+        for(i = 0; i < tmp.length; i++){
+            int line = i / brickOnLine;
+            if(line == lineCnt)
+                break;
+            int posX = i % brickOnLine;
+            double x = posX * brickLen;
+            x =(line % 2 == 0) ? x : (x - (brickLen / 2));
+            double y = (line) * brickHgt;
+            p.setLocation(x,y);
+
+            boolean b = ((line % 2 == 0 && i % 2 == 0) || (line % 2 != 0 && posX > centerLeft && posX <= centerRight));
+            tmp[i] = b ?  makeBrick(p,brickSize,typeA) : makeBrick(p,brickSize,typeB);
+        }
+
+        for(double y = brickHgt;i < tmp.length;i++, y += 2*brickHgt){
+            double x = (brickOnLine * brickLen) - (brickLen / 2);
+            p.setLocation(x,y);
+            tmp[i] = makeBrick(p,brickSize,typeA);
+        }
+        return tmp;
+    }
+
+    private void makeBall(Point2D ballPos){
+        setBall(new RubberBall(ballPos));
+    }
+
+    private Brick[][] makeLevels(Rectangle drawArea,int brickCount,int lineCount,double brickDimensionRatio){
+        Brick[][] tmp = new Brick[LEVELS_COUNT][];
+        tmp[0] = makeSingleTypeLevel(drawArea,brickCount,lineCount,brickDimensionRatio,CLAY);
+        tmp[1] = makeChessboardLevel(drawArea,brickCount,lineCount,brickDimensionRatio,CLAY,CEMENT);
+        tmp[2] = makeChessboardLevel(drawArea,brickCount,lineCount,brickDimensionRatio,CLAY,STEEL);
+        tmp[3] = makeChessboardLevel(drawArea,brickCount,lineCount,brickDimensionRatio,STEEL,CEMENT);
+        return tmp;
+    }
+
+    public void move(){
+        getPlayer().move();
+        getBall().move();
+    }
+
+    public void findImpacts(){
+        if(getPlayer().impact(getBall())){
+            getBall().reverseY();
+        }
+        else if(impactWall()){
+            /*for efficiency reverse is done into method impactWall
+            * because for every brick program checks for horizontal and vertical impacts
+            */
+            brickCount--;
+        }
+        else if(impactBorder()) {
+            getBall().reverseX();
+        }
+        else if(getBall().getPosition().getY() < area.getY()){
+            getBall().reverseY();
+        }
+        else if(getBall().getPosition().getY() > area.getY() + area.getHeight()){
+            ballCount--;
+            ballLost = true;
+        }
+    }
+
+    private boolean impactWall(){
+        for(Brick b : getBricks()){
+            switch(b.findImpact(getBall())) {
+                //Vertical Impact
+                case Brick.UP_IMPACT:
+                    getBall().reverseY();
+                    return b.setImpact(getBall().down, Brick.Crack.UP);
+                case Brick.DOWN_IMPACT:
+                    getBall().reverseY();
+                    return b.setImpact(getBall().up,Brick.Crack.DOWN);
+
+                //Horizontal Impact
+                case Brick.LEFT_IMPACT:
+                    getBall().reverseX();
+                    return b.setImpact(getBall().right,Brick.Crack.RIGHT);
+                case Brick.RIGHT_IMPACT:
+                    getBall().reverseX();
+                    return b.setImpact(getBall().left,Brick.Crack.LEFT);
+            }
+        }
+        return false;
+    }
+
+    private boolean impactBorder(){
+        Point2D p = getBall().getPosition();
+        return ((p.getX() < area.getX()) ||(p.getX() > (area.getX() + area.getWidth())));
+    }
+
+    public int getBrickCount(){
+        return brickCount;
+    }
+
+    public int getBallCount(){
+        return ballCount;
+    }
+
+    public boolean isBallLost(){
+        return ballLost;
+    }
+
+    public void ballReset(){
+        getPlayer().moveTo(startPoint);
+        getBall().moveTo(startPoint);
+        int speedX,speedY;
+        do{
+            speedX = rnd.nextInt(5) - 2;
+        }while(speedX == 0);
+        do{
+            speedY = -rnd.nextInt(3);
+        }while(speedY == 0);
+
+        getBall().setSpeed(speedX,speedY);
+        ballLost = false;
+    }
+
+    public void wallReset(){
+        for(Brick b : getBricks())
+            b.repair();
+        brickCount = getBricks().length;
+        ballCount = 3;
+    }
+
+    public boolean ballEnd(){
+        return ballCount == 0;
+    }
+
+    public boolean isDone(){
+        return brickCount == 0;
+    }
+
+    public void nextLevel(){
+        setBricks(levels[level++]);
+        this.brickCount = getBricks().length;
+    }
+
+    public boolean hasLevel(){
+        return level < levels.length;
+    }
+
+    public void setBallXSpeed(int s){
+        getBall().setXSpeed(s);
+    }
+
+    public void setBallYSpeed(int s){
+        getBall().setYSpeed(s);
+    }
+
+    public void resetBallCount(){
+        ballCount = 3;
+    }
+
+    private Brick makeBrick(Point point, Dimension size, int type){
+        Brick out;
+        switch(type){
+            case CLAY:
+                out = new ClayBrick(point,size);
+                break;
+            case STEEL:
+                out = new SteelBrick(point,size);
+                break;
+            case CEMENT:
+                out = new CementBrick(point, size);
+                break;
+            default:
+                throw  new IllegalArgumentException(String.format("Unknown Type:%d\n",type));
+        }
+        return  out;
+    }
+
+	public Player getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
+
+	public Ball getBall() {
+		return ball;
+	}
+
+	public void setBall(Ball ball) {
+		this.ball = ball;
+	}
+
+	public Brick[] getBricks() {
+		return bricks;
+	}
+
+	public void setBricks(Brick[] bricks) {
+		this.bricks = bricks;
+	}
+
+}
