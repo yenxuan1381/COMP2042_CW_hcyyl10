dfc8fb1cd7b1c62c5b386f11a515b030eebe60a1
yenxuan1381 <emilylo1381@gmail.com>
2021-12-08 21:16:15 +0800

    updated javadocs

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"main.java.gameWindow","l":"DebugPanel"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"main.java.gameWindow","l":"GameBoard"}]
