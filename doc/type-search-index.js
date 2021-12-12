8d4d0aec738b12db2dab04c2c871ff6fd4bab4fe
yenxuan1381 <emilylo1381@gmail.com>
2021-12-12 20:11:29 +0800

    regenerated javadocs

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"main.java.view","l":"CrackView"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"main.java.gameWindow","l":"GameBoard"}]
